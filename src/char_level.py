558b42815b2e424ab6f56f4ebf6d5131193ee7e1
teranixbq <hanieffathulb03@gmail.com>
2026-05-15 18:20:55 +0700

    update main.py clear dump text

--- src/char_level.py
+++ src/char_level.py
@@ -92,16 +92,12 @@ class HierarchicalCharCNN(nn.Module):
         kernel_sizes=None,
         num_classes=2,
         dropout=0.5,
-        pooling_type="max",
-        adaptive_output_size=4,
-        sequence_length=500,
     ):
         super().__init__()
 
         if kernel_sizes is None:
             kernel_sizes = [3, 5, 7]
 
-        self.pooling_type = pooling_type
         self.embedding = nn.Embedding(char_vocab_size, char_embed_dim, padding_idx=0)
         self.convs = nn.ModuleList()
 
@@ -117,20 +113,8 @@ class HierarchicalCharCNN(nn.Module):
             )
             in_channels = num_filters
 
-        self.adaptive_pool = nn.AdaptiveMaxPool1d(adaptive_output_size)
-
-        if pooling_type == "adaptive":
-            fc_input_dim = num_filters * adaptive_output_size
-        elif pooling_type == "none":
-            output_length = sequence_length
-            for kernel_size in kernel_sizes:
-                output_length = _conv1d_output_length(output_length, kernel_size)
-            fc_input_dim = num_filters * output_length
-        else:
-            fc_input_dim = num_filters
-
         self.dropout = nn.Dropout(dropout)
-        self.fc = nn.Linear(fc_input_dim, num_classes)
+        self.fc = nn.Linear(num_filters, num_classes)
 
     def forward(self, x):
         x = self.embedding(x).transpose(1, 2)
@@ -138,20 +122,7 @@ class HierarchicalCharCNN(nn.Module):
         for conv in self.convs:
             x = F.relu(conv(x))
 
-        if self.pooling_type == "max":
-            x = F.max_pool1d(x, kernel_size=x.shape[2]).squeeze(2)
-        elif self.pooling_type == "avg":
-            x = F.avg_pool1d(x, kernel_size=x.shape[2]).squeeze(2)
-        elif self.pooling_type == "adaptive":
-            x = self.adaptive_pool(x)
-            x = x.reshape(x.size(0), -1)
-        elif self.pooling_type == "none":
-            x = x.reshape(x.size(0), -1)
-        else:
-            raise ValueError(
-                "pooling_type harus 'max', 'avg', 'adaptive', atau 'none'"
-            )
-
+        x = F.max_pool1d(x, kernel_size=x.shape[2]).squeeze(2)
         x = self.dropout(x)
 
         return self.fc(x)
